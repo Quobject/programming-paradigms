c21e05918ff39a51a6e70a02cfc05c3e501b8671
Matthias <mludwig@quobject.io>
2017-03-02 11:20:04 +1100

    added install.sh

--- install.sh
+++ install.sh
@@ -1,11 +1,11 @@
 # prolog
-sudo apt-get install software-properties-common
+sudo apt-get -y install software-properties-common
 sudo apt-add-repository ppa:swi-prolog/stable
-sudo apt-get update
-sudo apt-get install swi-prolog
+sudo apt-get -y update
+sudo apt-get -y install swi-prolog
 
 # mono
 sudo apt-key adv --keyserver hkp://keyserver.ubuntu.com:80 --recv-keys 3FA7E0328081BFF6A14DA29AA6A19B38D3D831EF
 echo "deb http://download.mono-project.com/repo/debian wheezy main" | sudo tee /etc/apt/sources.list.d/mono-xamarin.list
-sudo apt-get update
-sudo apt-get install mono-devel
+sudo apt-get -y update
+sudo apt-get -y install mono-devel
